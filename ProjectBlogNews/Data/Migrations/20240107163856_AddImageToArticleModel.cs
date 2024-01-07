9355bd4e33f3956e889decb97e8135a6ee791106
Mateusz Koczorowski <mat.kocz00@gmail.com>
2024-01-07 21:56:20 +0100

    add bacgkround, logo. Change styling. Add image to article

---
+++ ProjectBlogNews/Data/Migrations/20240107163856_AddImageToArticleModel.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ProjectBlogNews.Data.Migrations
+{
+    public partial class AddImageToArticleModel : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "AltText",
+                table: "Article",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<string>(
+                name: "ImageFileName",
+                table: "Article",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "AltText",
+                table: "Article");
+
+            migrationBuilder.DropColumn(
+                name: "ImageFileName",
+                table: "Article");
+        }
+    }
+}
